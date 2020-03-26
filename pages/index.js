b826da1c7ffe3ed9880bc98a79fd9aa143834eea
Jonathan Grim <jonjongrim@gmail.com>
2020-03-26 18:48:52 -0400

    fix redirect URI

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ const PINTEREST_APP_ID = '5089674465726480473';
 
 const Home = () => (
   <a
-    href={`https://api.pinterest.com/oauth/?response_type=code&redirect_uri=https://localhost:3000/oauth&client_id=${PINTEREST_APP_ID}&scope=read_public&state=mysupersecretstate`}>
+    href={`https://api.pinterest.com/oauth/?response_type=code&redirect_uri=https://pinterest-slideshow.now.sh/oauth&client_id=${PINTEREST_APP_ID}&scope=read_public&state=mysupersecretstate`}>
     Sign in
   </a>
 );
