e01bc832e1798b5c4be791c504c12aeba9ced5cb
Jonathan Grim <jonjongrim@gmail.com>
2020-03-26 20:49:12 -0400

    fix token method

--- pages/oauth.tsx
+++ pages/oauth.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const OAuth = ({ accessToken }) => {
-  if (accessToken) {
+  if (accessToken && window) {
     sessionStorage.setItem('pinterestAccessToken', accessToken);
     return <div>You are now signed in</div>;
   }
@@ -12,9 +12,16 @@ export async function getServerSideProps(context) {
   const { query: { code = '' } = {} } = context;
   let accessToken = '';
   if (code) {
-    accessToken = await axios.get(
-      `https://api.pinterest.com/v1/oauth/token?grant_type=authorization_code&client_id=${process.env.PINTEREST_APP_ID}&client_secret=${process.env.PINTEREST_APP_SECRET}&code=${code}`
-    );
+    const res = await axios
+      .post(
+        `https://api.pinterest.com/v1/oauth/token?grant_type=authorization_code&client_id=${process.env.PINTEREST_APP_ID}&client_secret=${process.env.PINTEREST_APP_SECRET}&code=${code}`
+      )
+      .catch((err) => {
+        console.error(err);
+      });
+    if (res) {
+      accessToken = res.data;
+    }
   }
   return { props: { accessToken } };
 }
